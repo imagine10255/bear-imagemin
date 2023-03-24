b8fe7e151710e9621e98006386002444ec844549
imagine10255 <imagine10255@gmail.com>
2023-03-24 15:56:41 +0800

    調整

--- packages/bear-imagemin-server/src/main.ts
+++ packages/bear-imagemin-server/src/main.ts
@@ -3,7 +3,9 @@ import {AppModule} from './app.module';
 import * as fs from 'fs';
 import {tmpPath} from './config';
 
+
 async function bootstrap() {
+    console.log(`${process.env.npm_package_name} version: ${process.env.npm_package_version}`);
     const app = await NestFactory.create(AppModule);
 
     if (!fs.existsSync(tmpPath)) {
